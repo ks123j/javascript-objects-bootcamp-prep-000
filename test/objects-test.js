817281e0aed1f61a520ef2d66936147db01f85d9
Katy <komal.vadukul@gmail.com>
2017-10-27 02:38:05 +0000

    automatically backed up by learn

--- test/objects-test.js
+++ test/objects-test.js
@@ -17,7 +17,7 @@ describe('objects', () => {
 
   describe('updatePlaylist(playlist, artistName, songTitle)', () => {
     it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
-      expect(updatePlaylist({hridan: 'plays'}, 'satish jadhav', "works php")).
+      expect(updatePlaylist({hridan: 'plays'}, 'satish', "works php")).
         to.eql({"satish jadhav": "works php" })
     })
   })
